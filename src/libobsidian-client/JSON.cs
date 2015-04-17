dcc5224898bd802717e3b27c20865d071b03aa31
SplittyDev <splittydev@gmail.com>
2015-04-17 12:44:49 +0200

    Initial commit

---
+++ src/libobsidian-client/JSON.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace libObsidian.Client
+{
+	/// <summary>
+	/// Helper class for simple JSON deserialization
+	/// </summary>
+	public static class JSON
+	{
+		public static JsonResponse ParseResponse (string json) {
+			var serializer = new DataContractJsonSerializer (typeof(JsonResponse));
+			var data = Encoding.UTF8.GetBytes (json);
+			JsonResponse response;
+			using (var ms = new MemoryStream (data))
+				response = serializer.ReadObject (ms) as JsonResponse;
+			return response;
+		}
+
+		#if DEBUG
+		public static string Serialize (this JsonResponse json) {
+			var serializer = new DataContractJsonSerializer (typeof(JsonResponse));
+			string result;
+			using (var ms = new MemoryStream ()) {
+				serializer.WriteObject (ms, json);
+				result = Encoding.UTF8.GetString (ms.ToArray ());
+			}
+			return result;
+		}
+		#endif
+	}
+}
+
---
+++ src/obsidian-cli/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using libObsidian.Client;
+
+namespace obsidiancli
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Obsidian Command Line Interface");
+			var data = JsonResponse.Factory.Create (
+				Status.success,
+				"Image upload successful.",
+				new Dictionary<string, string> {
+					{ "filename", "xihF1GVd.jpeg" },
+					{
+						"url",
+						"http://exp.nikx.io/img/xihF1GVd.jpeg"
+					}
+				}
+			);
+			var json = data.Serialize ();
+			var response = JSON.ParseResponse (json);
+			Console.WriteLine ("[INPUT]\n{0}", json);
+			Console.WriteLine ("[OUTPUT]");
+			Console.WriteLine ("Status: {0}", response.Status);
+			Console.WriteLine ("Message: {0}", response.Message);
+			Console.WriteLine ("Data:");
+			foreach (var kvp in response.Data)
+				Console.WriteLine (" {0}: {1}", kvp.Key, kvp.Value);
+		}
+	}
+}
