97425f679502699423ef82cb878c4c09be0fae7f
Splitty <splittydev@gmail.com>
2015-05-18 04:52:13 +0200

    Added HttpMethod enumeration

---
+++ src/libobsidian-client/Enumerations/HttpMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace libObsidian.Client
+{
+	/// <summary>
+	/// HTTP method.
+	/// </summary>
+	public enum HttpMethod {
+		
+		/// <summary>
+		/// HTTP GET
+		/// </summary>
+		GET,
+
+		/// <summary>
+		/// HTTP PUT
+		/// </summary>
+		PUT,
+
+		/// <summary>
+		/// HTTP POST
+		/// </summary>
+		POST,
+
+		/// <summary>
+		/// HTTP DELETE
+		/// </summary>
+		DELETE,
+	}
+}
+
